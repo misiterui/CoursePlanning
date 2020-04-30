2d2fb6e6413af9c6ff0a7facdec38d1badfc524b
Chenyu Li <cla300@sfu.ca>
2020-04-30 00:39:39 -0700

    Add read and write method to store data in txt file
    Try to store added courses information in a txt file, so that the course objects can be stored permanently.

--- CoursePlanningApp/app/src/main/java/com/example/courseplanningapp/model/CourseManager.java
+++ CoursePlanningApp/app/src/main/java/com/example/courseplanningapp/model/CourseManager.java
@@ -9,10 +9,21 @@ import android.widget.Toast;
 import androidx.annotation.RequiresApi;
 
 import com.example.courseplanningapp.R;
+import com.example.courseplanningapp.constants.Constants;
+import com.example.courseplanningapp.ui.CourseList;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.RandomAccessFile;
+import java.nio.Buffer;
 import java.nio.charset.StandardCharsets;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -28,7 +39,7 @@ import java.util.Locale;
 public class CourseManager {
     private ArrayList<Course> courses = new ArrayList<>();
     private ArrayList<Course> filteredCourses = null;
-    private ArrayList<String> addedCourseId = new ArrayList<>();
+    private ArrayList<String> removedCourseId = new ArrayList<>();
 
     // Singleton
     private static CourseManager instance;
@@ -36,11 +47,11 @@ public class CourseManager {
     // read the course data file
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     private CourseManager(Context context) throws IOException {
-        loadAddedCourses(context);
+        //loadCourseInfoFromFile(context);
         InputStreamReader isr;
         try {
             isr = new InputStreamReader(
-                    context.openFileInput("select_courses.txt"),
+                    context.openFileInput(Constants.SAVE_DATA_FILENAME),
                     StandardCharsets.UTF_8);
         } catch (IOException e){
             isr = new InputStreamReader(
@@ -54,17 +65,29 @@ public class CourseManager {
         courses.clear();
 
         String line;
-
-        // read the first line first so skip the title
-        reader.readLine();
+        String year, semester, subject, courseNumber, title;
 
         // iteratively read the whole file
         while((line = reader.readLine()) != null) {
-            String[] courseData = line.split(",");
-            //courses.add(new Course());
-            System.out.println("标识："+line);
+            String[] courseInfo = line.split(",");
+//            if(!Boolean.parseBoolean(courseInfo[5])){
+//                continue;
+//            }
+            System.out.println("line里面是：" + line);
+            System.out.println("courseInfo里面是：" + courseInfo);
+            year = courseInfo[0];
+            semester = courseInfo[1];
+            subject = courseInfo[2];
+            courseNumber = courseInfo[3];
+            title = courseInfo[4];
+            Course course = new Course(year, semester, subject, courseNumber, title);
+            courses.add(course);
+            System.out.println("此时的course是：" + course.toString());
+            System.out.println("此时的courses是：" + courses.toString());
         }
 
+
+        System.out.println("标识："+line);
 
         Collections.sort(courses);
 
@@ -80,33 +103,94 @@ public class CourseManager {
 
     public void removeCourse(Course course) {
         courses.remove(course);
+        removedCourseId.add(course.getCourseId());
     }
 
-    public void saveAddedCourses(Context context) {
-        SharedPreferences preferences = context.getSharedPreferences("sharedPreference", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = preferences.edit();
 
+    // save new added course to txt file
+    public void saveCourseInfoToFile(Context context, Course course, String filename){
+        String filePath = "/sdcard/Gyt/";
+        String fileName = Constants.SAVE_DATA_FILENAME;
+        FileOutputStream fileOutputStream = null;
+        BufferedWriter bufferedWriter;
         StringBuilder serializedCourses = new StringBuilder();
-        for (Course course : courses) {
-
-            for (int i = 0; i < courses.size(); i++) {
-                String courseId = courses.get(i).getCourseId();
-                serializedCourses.append(courseId);
-                serializedCourses.append(",");
-
+        try {
+            // MODE_APPEND - if a txt file with the same name, then append this file
+            fileOutputStream = context.openFileOutput(fileName, Context.MODE_APPEND);
+            bufferedWriter = new BufferedWriter(new OutputStreamWriter(fileOutputStream));
+            serializedCourses.append(course.getYear());
+            serializedCourses.append(",");
+            serializedCourses.append(course.getSemester());
+            serializedCourses.append(",");
+            serializedCourses.append(course.getSubject());
+            serializedCourses.append(",");
+            serializedCourses.append(course.getCourseNumber());
+            serializedCourses.append(",");
+            serializedCourses.append(course.getTitle());
+            serializedCourses.append(",");
+            serializedCourses.append(course.getVisible());
+            serializedCourses.append("\n");
+            String data = serializedCourses.toString();
+            fileOutputStream.write(data.getBytes());
+            System.out.println("写入成功，内容为："+ data);
+        }catch (IOException e){
+            Toast.makeText(context, "Sorry, there's an error on saving this data", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        } finally {
+            if (fileOutputStream != null) {
+                try {
+                    fileOutputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-            editor.putString("AddedCourseId", serializedCourses.toString());
-            editor.commit();
         }
     }
 
-    public void loadAddedCourses(Context context) {
-        SharedPreferences preferences = context.getSharedPreferences("sharedPreference", Context.MODE_PRIVATE);
-        String serializedCourses = preferences.getString("AddedCourseId", "");
-        String[] serializedCoursesId = serializedCourses.split(",");
-        for (String courseId: serializedCoursesId){
-            if (courseId != null && !courseId.isEmpty()) {
-                addedCourseId.add(courseId);
+    // read data from txt file
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void loadCourseInfoFromFile(Context context) {
+        FileInputStream fileInputStream;
+        BufferedReader bufferedReader = null;
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            fileInputStream = context.openFileInput(Constants.SAVE_DATA_FILENAME);
+            bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream, StandardCharsets.UTF_8));
+            String result = "";
+            while((result = bufferedReader.readLine()) != null) {
+                stringBuilder.append(result);
+            }
+            fileInputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        courses.clear();
+        String[] courseInformation;
+        String year, semester, subject, courseNumber, title;
+        String[] allCourseInfo = stringBuilder.toString().split("\n");
+        if (stringBuilder != null && !stringBuilder.toString().isEmpty()) {
+            for (String courseInfo : allCourseInfo) {
+                System.out.println("courseInfo里面是：" + courseInfo);
+                courseInformation = courseInfo.split(",");
+                System.out.println("courseInformation里面是：" + courseInformation[0]);
+                year = courseInformation[0];
+                semester = courseInformation[1];
+                subject = courseInformation[2];
+                courseNumber = courseInformation[3];
+                title = courseInformation[4];
+                Course course = new Course(year, semester, subject, courseNumber, title);
+                courses.add(course);
+                System.out.println("此时的course是：" + course.toString());
+                System.out.println("此时的courses是：" + courses.toString());
             }
         }
     }
